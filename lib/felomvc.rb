1f93ec0dd831cdfe1dce81c0db0afd92eb2dbfbb
Juan Felipe Arango <jfelipe38@gmail.com>
2016-09-21 14:05:09 -0500

    Better routing

--- lib/felomvc.rb
+++ lib/felomvc.rb
@@ -2,48 +2,16 @@ require "felomvc/version"
 require "felomvc/controller.rb"
 require "felomvc/utils.rb"
 require "felomvc/dependencies.rb"
+require "felomvc/routing.rb"
 
 module Felomvc
-
   class Application
-
-    #This call will be 'called' every time a new instance of Application is created
     def call(env)
-
-      #Root path
-
-      if env["PATH_INFO"] == "/"
-        return [302, {"Location" => "pages/about"}, []]
-      end
-
-      #Respong 500 to the favicon request
-
       if env["PATH_INFO"] == "/favicon.ico"
-        return [500, {}, []]
+        return [ 500, { }, [] ]
       end
-
-      # env["PATH_INFO"] = "/pages/about" => PagesController.send(:about)
-      controller_class, action = get_controller_and_action(env)
-
-      controller = controller_class.new(env)
-
-      #with the controller name and the action, this line sends the action to the controller name
-      response = controller.send(action)
-    end
-
-    def get_controller_and_action(env)
-      #From path info it grabs the information of the controller name and the action to be send
-      _, controller_name, action = env["PATH_INFO"].split("/")
-
-      #Grabs the controller name from the route, convert it to camel case and adds the controller word to follow the conventions
-      #Example route /my_pages/about => MyPagesController
-      controller_name = controller_name.to_camel_case + "Controller"
-
-      #Returns an array with the controller name constantized and the action
-      #This change is made because it is no possible to create instances of strings.
-      [ Object.const_get(controller_name), action ]
+      get_rack_app(env).call(env)
     end
 
   end
-
 end
